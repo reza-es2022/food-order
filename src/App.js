381a5ce815b137aae5dfc7662d4ef40a844978a8
Reza Esmaili <r.esmaili2022@gmail.com>
2024-08-14 06:23:56 +0330

    add cart visibily fucntion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
+import { useState } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [isCartShown, setIsCartShown] = useState(false);
   return (
     <CartProvider>
+      {isCartShown && <div>Cart...</div>}
       <Header />
       <main>
         <Meals />
