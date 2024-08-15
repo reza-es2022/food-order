23fccfd3f1ebd22e279611abf45f5fc8e578aeb0
Reza Esmaili <r.esmaili2022@gmail.com>
2024-08-15 09:51:26 +0330

    create cart ui and add close feature to it

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import CartProvider from "./store/CartProvider";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   const [isCartShown, setIsCartShown] = useState(false);
   return (
     <CartProvider>
-      {isCartShown && <div>Cart...</div>}
+      {isCartShown && <Cart onClose={() => setIsCartShown(false)} />}
       <Header onShowCart={() => setIsCartShown(true)} />
       <main>
         <Meals />
